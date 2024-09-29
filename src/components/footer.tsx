4f3c8588a488f9653f6196b34c67e593f921dd75
invadoli <id67745@ubt-uni.net>
2024-09-29 19:48:12 +0200

    Footer Responsivity

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'; 
 import Link from 'next/link';
 import purpleClouds from '../images/purpleClouds.png';
+import mobileCloudImage from '../images/cloudResponsive.png';
 import logo from '../images/logo2.png';
 import { FaFacebookF, FaInstagram } from 'react-icons/fa';
-import { Poppins} from 'next/font/google';
+import { Poppins } from 'next/font/google';
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -13,10 +14,22 @@ const poppins = Poppins({
 export default function Footer() {
   return (
     <>
-      <div className='mt-32'>
-        <Image src={purpleClouds} alt="cloud"/>
+      <div className="mt-32 relative">
+         <div className="hidden md:block">
+           <Image
+             src={purpleClouds}
+             alt="cloud for desktop"
+           />
+         </div>
+         <div className="block md:hidden absolute -z-10 -mt-[100px]">
+           <Image
+             src={mobileCloudImage}
+             alt="cloud for mobile"
+             className="w-[1980px] h-auto"
+           />
+         </div>
       </div>
-      <div className={`bg-[#37008E] h-[416px] flex flex-col justify-between${poppins.className}`}>
+      <div className={`bg-[#37008E] md:h-[416px] flex flex-col justify-between ${poppins.className}`}>
         <div className="flex flex-col justify-between items-center h-full">
           
           <div className="mb-6 md:mb-0">
@@ -25,33 +38,35 @@ export default function Footer() {
               alt="Logo"
               width={300} 
               height={122} 
+              className="w-[299.16px] md:w-[300px]"
             />
           </div>
 
           <div className="flex space-x-4 mb-6 md:mb-0 mt-10">
             <a href="https://www.instagram.com" target="_blank">
               <div className="bg-white rounded-full w-[30px] h-[30px] flex items-center justify-center">
-                <FaInstagram className="text-2xl hover:text-pink-500" />
+                <FaInstagram className="text-2xl text-[#37008E] hover:text-pink-500" />
               </div>
             </a>
             <a href="https://www.facebook.com" target="_blank">
               <div className="bg-white rounded-full w-[30px] h-[30px] flex items-center justify-center">
-                <FaFacebookF className="text-2xl hover:text-blue-600" />
-                </div>
+                <FaFacebookF className="text-2xl text-[#37008E] hover:text-blue-600" />
+              </div>
             </a>
           </div>
 
-          <nav className='flex flex-row gap-12 text-white mb-4 mt-10'> 
+          {/* Centered navigation links */}
+          <nav className='grid grid-cols-3 gap-4 text-white mb-4 mt-6 md:mt-10 justify-center items-center md:flex md:flex-row md:gap-12 md:justify-center'> 
             <Link href="/books">Books</Link>
             <Link href="/features">Features</Link>
             <Link href="/howitworks">How it works</Link>
-            <Link href="/about">About us</Link>
-            <Link href="/register">Register</Link>
-            <Link href="/login">Login</Link>
+            <Link href="/about" className="text-center">About us</Link>
+            <Link href="/register" className="text-center">Register</Link>
+            <Link href="/login" className="text-center">Login</Link>
           </nav>
 
-          <hr className=" border-[#FFFFFF33] w-full mb-4" />
-          <div className="text-[#FFFFFF33] text-center mb-4" style={{fontSize: '14px'}}>
+          <hr className="border-[#FFFFFF33] w-[90%] md:w-full mb-4" />
+          <div className="text-[#FFFFFF33] text-center mb-4 text-xs md:text-sm">
             Copyrights @2024: Brain Hero - Our cool Artificial Intelligence friend
           </div>
         </div>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -17,84 +17,87 @@ const Header: React.FC = () => {
 
   return (
     <header>
-      <nav className="p-2 bg-[#008EA7]">
-        <div className="mx-auto max-w-[1200px] px-4">
-          <div className={`hidden md:flex justify-end gap-2 mb-2 ${isMobileMenuOpen ? 'hidden' : ''}`}>
-            <Link href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-              <FaInstagram size={20} className="text-white" />
-            </Link>
-            <Link href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-              <FaFacebook size={20} className="text-white" />
-            </Link>
+  <nav className="p-2 bg-[#008EA7]">
+    <div className="mx-auto max-w-[1000px] px-2">
+      <div className={`hidden md:flex justify-end gap-2 mb-2 ${isMobileMenuOpen ? 'hidden' : ''}`}>
+        <Link href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+          <div className="bg-white rounded-full ">
+            <FaInstagram size={22} className="text-[#008EA7] transform scale-75" />
           </div>
+        </Link>
+        <Link href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+          <FaFacebook size={22} className="text-white" />
+        </Link>
+      </div>
 
-          <div className={`flex justify-between items-center ${isMobileMenuOpen ? 'hidden' : ''}`}>
-            <div>
-              <Link href="/">
-                <Image src={logo} alt="BrainHero Logo" width={192.6} height={89.73} />
-              </Link>
-            </div>
-
-            <div className="hidden md:flex items-center gap-7 text-white">
-              <Link href="/">Home</Link>
-              <Link href="/books">Books</Link>
-              <Link href="/features">Features</Link>
-              <Link href="/howitworks">How it works</Link>
-              <Link href="/about">About us</Link>
-
-              <Link href="/login">
-                <button className="px-8 py-2 bg-[#FA5E55] text-white rounded-full font-bold">
-                  Log In
-                </button>
-              </Link>
-            </div>
-
-            {/* Responsivity */}
-            <div className="md:hidden flex items-center">
-              <button onClick={toggleMobileMenu}>
-                {isMobileMenuOpen ? (
-                  <AiOutlineClose size={30} className="text-white" />
-                ) : (
-                  <GiHamburgerMenu size={30} className="text-white" />
-                )}
-              </button>
-            </div>
-          </div>
+      <div className={`flex justify-between items-center ${isMobileMenuOpen ? 'hidden' : ''}`}>
+        <div>
+          <Link href="/">
+            <Image src={logo} alt="BrainHero Logo" width={192.6} height={89.73} />
+          </Link>
         </div>
 
-        {isMobileMenuOpen && (
-          <div className="flex flex-col items-center bg-[#008EA7] h-screen pt-4 relative">
-            <button onClick={toggleMobileMenu} className="absolute top-4 right-6">
-              <AiOutlineClose size={30} className="text-white" />
+        <div className="hidden md:flex items-center gap-12 text-white">
+          <Link href="/" className="text-sm">Home</Link>
+          <Link href="/books" className="text-sm">Books</Link>
+          <Link href="/features" className="text-sm">Features</Link>
+          <Link href="/howitworks" className="text-sm">How it works</Link>
+          <Link href="/about" className="text-sm">About us</Link>
+
+          <Link href="/login">
+            <button className="px-6 py-2 bg-[#FA5E55] text-white rounded-full text-sm font-bold">
+              Log In
             </button>
+          </Link>
+        </div>
 
-            <Image src={logo} alt="BrainHero Logo" width={192.6} height={89.73} className="mt-10"/>
-            <nav className="flex flex-col items-center gap-12 text-white mt-20">
-              <Link href="/" onClick={toggleMobileMenu}>Home</Link>
-              <Link href="/books" onClick={toggleMobileMenu}>Books</Link>
-              <Link href="/features" onClick={toggleMobileMenu}>Features</Link>
-              <Link href="/howitworks" onClick={toggleMobileMenu}>How it works</Link>
-              <Link href="/about" onClick={toggleMobileMenu}>About us</Link>
+        {/* Responsivity */}
+        <div className="md:hidden flex items-center">
+          <button onClick={toggleMobileMenu}>
+            {isMobileMenuOpen ? (
+              <AiOutlineClose size={30} className="text-white" />
+            ) : (
+              <GiHamburgerMenu size={30} className="text-white" />
+            )}
+          </button>
+        </div>
+      </div>
+    </div>
 
-              <Link href="/login">
-                <button className="px-8 py-2 bg-[#FA5E55] text-white rounded-full font-bold">
-                  Log In
-                </button>
-              </Link>
-            </nav>
+    {isMobileMenuOpen && (
+      <div className="flex flex-col items-center bg-[#008EA7] h-auto pt-8 relative z-50 overflow-auto">
+        <button onClick={toggleMobileMenu} className="absolute top-4 right-6">
+          <AiOutlineClose size={30} className="text-white" />
+        </button>
 
-            <div className="flex gap-4 mt-8">
-              <Link href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-                <FaInstagram size={30} className="text-white" />
-              </Link>
-              <Link href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-                <FaFacebook size={30} className="text-white" />
-              </Link>
-            </div>
-          </div>
-        )}
-      </nav>
-    </header>
+        <Image src={logo} alt="BrainHero Logo" width={192.6} height={89.73} className="mt-8"/>
+        <nav className="flex flex-col items-center gap-8 text-white mt-10">
+          <Link href="/" onClick={toggleMobileMenu}>Home</Link>
+          <Link href="/books" onClick={toggleMobileMenu}>Books</Link>
+          <Link href="/features" onClick={toggleMobileMenu}>Features</Link>
+          <Link href="/howitworks" onClick={toggleMobileMenu}>How it works</Link>
+          <Link href="/about" onClick={toggleMobileMenu}>About us</Link>
+
+          <Link href="/login">
+            <button className="px-6 py-2 bg-[#FA5E55] text-white rounded-full text-sm font-bold">
+              Log In
+            </button>
+          </Link>
+        </nav>
+
+        <div className="flex gap-4 mt-8">
+          <Link href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            <FaInstagram size={24} className="text-white" />
+          </Link>
+          <Link href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+            <FaFacebook size={24} className="text-white" />
+          </Link>
+        </div>
+      </div>
+    )}
+  </nav>
+</header>
+
   );
 };
 
